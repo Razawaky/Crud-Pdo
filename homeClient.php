e6d84d75984d6de51de7051ba50bbf7af18e13ec
Razawaky <viniciusduarte564@gmail.com>
2022-12-01 11:56:45 -0300

    Fim

--- homeClient.php
+++ homeClient.php
@@ -1,9 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="client.css">
+</head>
+<body>
+    
+
 <?php
 
 include_once "conexao.php";
 try{
     $consulta = $conectar -> query("SELECT * FROM cliente");
-    echo "<a href='formClient.php'>Novo Cadastro</a><br><br><h1>Listagem de Usuários</h1>";
+    echo "<a href='formClient.php'><button> Novo Cadastro </button></a><a href='index.html'><button>Voltar</button></a><br><br><h1>Listagem de Usuários</h1> ";
     echo "<table border='1'<tr><td>Nome</td><td>Endereço</td><td>Cidade</td><td>CEP</td><td>Ações</td></tr>";
     while ($linha = $consulta->fetch(PDO::FETCH_ASSOC)){
         echo "<tr><td>$linha[nome]</td><td>$linha[endereco]</td><td>$linha[cidade]</td><td>$linha[cep]</td><td><a href='formEditarClient.php?cod_cliente=$linha[cod_cliente]'>Editar</a> - <a href='excluirClient.php?cod_cliente=$linha[cod_cliente]'>Excluir</a></td></tr>";
@@ -14,3 +25,6 @@ try{
     echo $e->getMessage();
     }
 ?>
+
+</body>
+</html>
--- homeVeterinario.php
+++ homeVeterinario.php
@@ -1,9 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="vet.css">
+</head>
+<body>
+    
+
 <?php
 
 include_once "conexao.php";
 try{
     $consulta = $conectar -> query("SELECT * FROM vet");
-    echo "<a href='formVeterinario.php'>Novo Cadastro</a><br><br><h1>Listagem de Usuários</h1>";
+    echo "<a href='formVeterinario.php'><button> Novo Cadastro</button></a><a href='index.html'><button>Voltar</button></a><br><br><h1>Listagem de Usuários</h1>";
     echo "<table border='1'<tr><td>Nome</td><td>CRM</td><td>CPF</td><td>Email</td><td>Ações</td></tr>";
     while ($linha = $consulta->fetch(PDO::FETCH_ASSOC)){
         echo "<tr><td>$linha[nome]</td><td>$linha[crm]</td><td>$linha[cpf]</td><td>$linha[email]</td> <td><a href='formEditarVeterinario.php?cod_vet=$linha[cod_vet]'>Editar</a> - <a href='excluirVeterinario.php?cod_vet=$linha[cod_vet]'>Excluir</a></td></tr>";
@@ -17,3 +28,5 @@ try{
 
 
 
+</body>
+</html>
